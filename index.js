d7276d7ee3565d221238fd99e8f95b964be01c60
Thomas Weichhart <multihacker007yt@gmail.com>
2021-03-18 23:43:28 +0100

    variable triggers

--- index.js
+++ index.js
@@ -6,16 +6,19 @@ const client = new ChatClient.ChatClient({
 });
 
 client.on("ready", () => {
-    console.log("Verbunden");
+    console.log("connected");
 });
 
 client.on("PRIVMSG", (msg) => {
-    if (msg.senderUsername != CONFIG.name) {
-        let dadNameStartPos = msg.messageText.toLowerCase().search("ich bin");
-        if (dadNameStartPos != -1) {
-            let dadname = msg.messageText.substr(dadNameStartPos + 8);
-            client.say(msg.channelName, `Hey ${dadname} , ich bin Dad :p, @${msg.displayName}`);
-            console.log(`[${msg.displayName}] in [${msg.channelName}]: ${msg.messageText}`);
+    if (msg.senderUsername != CONFIG.name) {    //if message is sent by bot (himself), ignore
+        for (let i = 0; i < CONFIG.triggers.length; i++) {
+            let dadNameStartPos = msg.messageText.toLowerCase().search(CONFIG.triggers[i]); //Search for trigger string in message, for example "im", or "i am"
+            if (dadNameStartPos != -1) {
+                let dadname = msg.messageText.substr(dadNameStartPos + CONFIG.triggers[i].length + 1);  //name is everything after the trigger string
+                client.say(msg.channelName, `Hey ${dadname} , I'm Dad :p, @${msg.displayName}`);        //chat
+                console.log(`[${msg.displayName}] in [${msg.channelName}]: ${msg.messageText}`);        //log in console
+                break;
+            }
         }
     }
 })
